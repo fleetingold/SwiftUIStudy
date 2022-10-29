ecebe55c60157321f32c8bf4a8c130d72776d1be
fleetingold <1161851523@qq.com>
2022-10-29 15:43:01 +0800

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by fleetingold on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
